cc6bcc79e3fd5352a984cb69cf2a5a87a52dd0ad
Luke Olali <mugapedia@gmail.com>
2021-11-27 23:59:16 +0300

    describes query for fetching user login and name

--- src/Profile/index.js
+++ src/Profile/index.js
@@ -1,3 +1,13 @@
+import { gql } from '@apollo/client'
+
+const GET_CURRENT_USER = gql`
+  {
+    viewer {
+      login
+      name
+    }
+  }
+
 const Profile = () => {
   return (
     <div>Profile</div>
