52719f435149726e2d269c1bb64b31a5cf69e6b4
Luke Olali <mugapedia@gmail.com>
2021-11-28 00:18:14 +0300

    displays the GitHub login and name using the Profiile component

--- src/Profile/index.js
+++ src/Profile/index.js
@@ -1,4 +1,4 @@
-import { gql } from '@apollo/client'
+import { gql, useQuery } from '@apollo/client'
 
 const GET_CURRENT_USER = gql`
   {
@@ -10,8 +10,16 @@ const GET_CURRENT_USER = gql`
 `
 
 const Profile = () => {
+  const { loading, error, data } = useQuery(GET_CURRENT_USER)
+
+  if(loading) return "Loading......"
+  if(error) return `Error: ${error.message}`
+  const { viewer } = data
   return (
-    <div>Profile</div>
+    <div>
+      <h1>Profile</h1>
+      {viewer.name} {viewer.login}
+    </div>
   )
 }
 
