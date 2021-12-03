13ba117709abf2e2ab5b0edd3b9fa14428f77b22
Luke Olali <mugapedia@gmail.com>
2021-12-03 23:41:28 +0300

    creates Button component

---
+++ src/Button/index.js
@@ -0,0 +1,10 @@
+const Button = ({ children, className, color='black', type='button', ...props }) => {
+  return (
+    <button className={`${className} Button Button_${color}`}
+            type={type}
+            {...props}
+    >{children}</button>
+  )
+}
+
+export default Button
