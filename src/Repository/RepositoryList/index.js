0aa82df720dd73b4f5a1dba432a9ee5544b99737
Luke Olali <mugapedia@gmail.com>
2022-01-10 17:37:01 +0300

    adds a button below the list of repositories to fetch more repos

--- src/Repository/RepositoryList/index.js
+++ src/Repository/RepositoryList/index.js
@@ -1,13 +1,28 @@
 import RepositoryItem from "../RepositoryItem"
 import '../../index.css'
 
-const RepositoryList = ({ repositories }) => {
+const RepositoryList = ({ repositories, fetchMore }) => {
   return (
-    repositories.edges.map(({ node }) => (
-      <div key={node.id} className="RepositoryItem">
-        <RepositoryItem {...node} />
-      </div>
-    ))
+    <>
+      {
+        repositories.edges.map(({ node }) => (
+          <div key={node.id} className="RepositoryItem">
+            <RepositoryItem {...node} />
+          </div>
+        ))
+      }
+
+      {
+        repositories.pageInfo.hasNexPage && (
+          <button
+            type="button"
+            onClick={() => {
+              fetchMore()
+            }}
+          >More Repos</button>
+        )
+      }
+    </>
   )
 }
 
