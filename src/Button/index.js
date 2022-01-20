5675b48efe609c6e18cfe7c488c50ca558788902
Luke Olali <mugapedia@gmail.com>
2022-01-20 15:43:55 +0300

    defines component representing a generic button

--- src/Button/index.js
+++ src/Button/index.js
@@ -9,4 +9,18 @@ const Button = ({ children, className, color='black', type='button', ...props })
   )
 }
 
+const ButtonUnobtrusive = ({ children, className, type='button', ...props }) => {
+  return (
+    <div className='FetchMore'>
+      <button
+        className={`${className} Button_unobtrusive`}
+        type={type}
+        {...props}
+      >{children}</button>
+    </div>
+  )
+}
+
+export { ButtonUnobtrusive }
+
 export default Button
