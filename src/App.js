41e853ef9dcb6a3e5831fe745388e40283b3ca33
Luke Olali <mugapedia@gmail.com>
2022-03-01 00:10:00 +0300

    renders different component based on path visited

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import './App.css';
 import Profile from './Profile';
+import Organization from './Organization';
+import { Routes, Route } from 'react-router-dom'
+import * as routes from './constants/routes'
 
 function App() {
   return (
-    <Profile />
+    <div className='App'>
+      <Routes>
+        <Route exact path={routes.ORGANIZATION} element={<Organization />} />
+        <Route path={routes.PROFILE} element={<Profile />} />
+      </Routes>
+    </div>
   );
 }
 
