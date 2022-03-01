a5500d91eea59b06a4ecf26fd5cf97652438ed0d
Luke Olali <mugapedia@gmail.com>
2022-03-01 23:48:49 +0300

    renders the Navigation component within App component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Profile from './Profile';
 import Organization from './Organization';
+import Navigation from './Navigation';
 import { Routes, Route } from 'react-router-dom'
 import * as routes from './constants/routes'
 
 function App() {
   return (
     <div className='App'>
+      <Navigation />
       <Routes>
         <Route exact path={routes.ORGANIZATION} element={<Organization />} />
         <Route path={routes.PROFILE} element={<Profile />} />
