8ed27b7864230781e9731d9ca5b49bfc78dd0079
Luke Olali <mugapedia@gmail.com>
2022-03-02 01:08:26 +0300

    adds header element to the jsx returned by Navigation compponent

--- src/Navigation/index.js
+++ src/Navigation/index.js
@@ -1,17 +1,22 @@
 import { Link } from 'react-router-dom'
+import * as routes from '../constants/routes'
+
+import './style.css'
 
 const Navigation = () => {
   return (
-    <nav>
-      <ul>
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="/profile">Profile</Link>
-        </li>
-      </ul>
-    </nav>
+    <header>
+      <nav>
+        <ul>
+          <li>
+            <Link to={routes.ORGANIZATION}>Home</Link>
+          </li>
+          <li>
+            <Link to={routes.PROFILE}>Profile</Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
   )
 }
 
