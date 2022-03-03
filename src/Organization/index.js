80570c465fc81111cd8e946bc8c9c25995ab1709
Luke Olali <mugapedia@gmail.com>
2022-03-04 00:39:44 +0300

    defines query that fetches an organization's repositories

--- src/Organization/index.js
+++ src/Organization/index.js
@@ -1,3 +1,22 @@
+import { gql } from "@apollo/client"
+import { REPOSITORY_FRAGMENT } from "../Repository"
+
+const GET_REPOSITORIES_OF_ORGANIZATION = gql`
+  ${REPOSITORY_FRAGMENT}
+  query($organizationLogin: String!) {
+    organization(login: $organizatinLogin) {
+      repositories(first: 5, orderBy: { direction: DESC, field: CREATED_AT }) {
+        edges {
+          node {
+            ...repository
+          }
+        }
+      }
+    }
+  }
+`
+
+
 const Organization = () => (
   <div className="App-content_large-header">
     <h1>Organization</h1>
