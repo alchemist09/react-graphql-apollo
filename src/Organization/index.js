3ca412fc3d84c6e7a196b26c87496209f897b648
Luke Olali <mugapedia@gmail.com>
2022-03-04 01:10:07 +0300

    defines body of Organization component as that which fetches an organizations repositories and displays them

--- src/Organization/index.js
+++ src/Organization/index.js
@@ -1,10 +1,13 @@
-import { gql } from "@apollo/client"
+import { gql, useQuery } from "@apollo/client"
 import { REPOSITORY_FRAGMENT } from "../Repository"
+import Loading from "../Loading"
+import ErrorMessage from "../Error"
+import RepositoryList from "../Repository"
 
 const GET_REPOSITORIES_OF_ORGANIZATION = gql`
   ${REPOSITORY_FRAGMENT}
   query($organizationLogin: String!) {
-    organization(login: $organizatinLogin) {
+    organization(login: $organizationLogin) {
       repositories(first: 5, orderBy: { direction: DESC, field: CREATED_AT }) {
         edges {
           node {
@@ -17,10 +20,31 @@ const GET_REPOSITORIES_OF_ORGANIZATION = gql`
 `
 
 
-const Organization = () => (
-  <div className="App-content_large-header">
-    <h1>Organization</h1>
-  </div>
-)
+const Organization = ({ organizationName }) => {
+  const { loading, error, data, fetchMore } = useQuery(GET_REPOSITORIES_OF_ORGANIZATION, { 
+    variables: {
+      login: organizationName
+    }
+  })
+
+  if(loading && !data) {
+    return <Loading />
+  }
+
+  if(error) {
+    return <ErrorMessage error={error} />
+  }
+
+  if(!data) { return null }
+
+  const { repositories } = data.repositories
+
+  return (
+    <div className="App-content_large-header">
+      <h1>Organization</h1>
+      <RepositoryList loading={loading} repositories={repositories} fetchMore={fetchMore} />
+    </div>
+  )
+}
 
 export default Organization
