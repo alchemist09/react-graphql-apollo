98ffb714dd5c90fcae11fb142796b14a9b89d91e
Luke Olali <mugapedia@gmail.com>
2022-05-02 00:26:17 +0300

    - refactors the Navigation component to manage state and props passed to OrganizationSearch component. - defines component that enables looking up organizations by their name

--- src/Navigation/index.js
+++ src/Navigation/index.js
@@ -1,10 +1,17 @@
+import { useState } from 'react'
 import { Link, useLocation } from 'react-router-dom'
 import * as routes from '../constants/routes'
 
 import './style.css'
 
 const Navigation = () => {
+  const [orgName, setOrgName] = useState('github')
   const { pathname } = useLocation()
+
+  const onOrganizationSearch = value => {
+    setOrgName(value)
+  }
+
   return (
     <header className="Navigation">
       <nav>
@@ -16,12 +23,35 @@ const Navigation = () => {
             <Link to={routes.PROFILE}>Profile</Link>
           </li>
         </ul>
-        {pathname === routes.ORGANIZATION && (<OrganizationSearch />)}
+        {pathname === routes.ORGANIZATION && (
+          <OrganizationSearch 
+            organizationName={orgName}
+            onOrganizationSearch={onOrganizationSearch} 
+          />
+        )}
       </nav>
     </header>
   )
 }
 
-const OrganizationSearch = () => <div>Search Organization</div>
+const OrganizationSearch = ({ organizationName, onOrganizationSearch }) => {
+  const [organization, setOrganization] = useState(organizationName)
+
+  const handleChange = evt => {
+    setOrganization(evt.target.value)
+  }
+
+  const handleSubmit = () => {
+    onOrganizationSearch(organization)
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <label>Organization</label>
+      <input type="text" id="org_name" value={organization} onChange={handleChange} />
+      <button type="submit">Search Repos</button>
+    </form>
+  )
+}
 
 export default Navigation
