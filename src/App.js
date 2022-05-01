252221f2b7eda04fa6004c7d68a5953fae3148fe
Luke Olali <mugapedia@gmail.com>
2022-05-02 01:35:20 +0300

    keeps track of current organization and passes the value down to Navigation adn Organization components

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import './App.css';
 import Profile from './Profile';
 import Organization from './Organization';
@@ -6,11 +7,21 @@ import { Routes, Route } from 'react-router-dom'
 import * as routes from './constants/routes'
 
 function App() {
+  const [orgName, setOrgName] = useState('github')
+
+  const onOrganizationSearch = (value) => {
+    setOrgName(value)
+  }
+
   return (
     <div className='App'>
-      <Navigation />
+      <Navigation
+        organizationName={orgName}
+        onOrganizationSearch={onOrganizationSearch}
+      />
+
       <Routes>
-        <Route exact path={routes.ORGANIZATION} element={<Organization />} />
+        <Route exact path={routes.ORGANIZATION} element={<Organization organizationLogin={orgName} />} />
         <Route path={routes.PROFILE} element={<Profile />} />
       </Routes>
     </div>
