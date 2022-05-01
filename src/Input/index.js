52b856264bad17c039c61b04cc98d021eb3c7a7e
Luke Olali <mugapedia@gmail.com>
2022-05-02 00:42:12 +0300

    defines a component that renders an input field on the page

---
+++ src/Input/index.js
@@ -0,0 +1,9 @@
+import './style.css'
+
+const Input = ({ children, color='black', ...props }) => (
+  <input className={`Input Input_${color}`} {...props}>
+    {children}
+  </input>
+)
+
+export default Input
