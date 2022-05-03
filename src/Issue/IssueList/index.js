48763cbcb90e18594f3dd8634098f568d9097055
Luke Olali <mugapedia@gmail.com>
2022-05-03 23:49:36 +0300

    defiines Issues component and query used to retrieve list of issues associated with a repo and repo owner

---
+++ src/Issue/IssueList/index.js
@@ -0,0 +1,37 @@
+import { useQuery, gql } from '@apollo/client'
+
+const GET_ISSUES_OF_REPOSITORY = gql`
+  query($repositoryName: String!, $repositoryOwner: String!) {
+    repository(name: $repositoryName, owner: $repositoryOwner) {
+      issues(first: 5) {
+        edges {
+          node {
+            id
+            bodyHTML
+            number
+            state
+            title
+            url
+          }
+        }
+      }
+    }
+  }
+`
+
+const Issues = ({ repositoryName, repositoryOwner }) => {
+  const { loadinig, error, data } = useQuery(GET_ISSUES_OF_REPOSITORY, {
+    variables: {
+      repositoryName,
+      repositoryOwner
+    }
+  })
+
+  return (
+    <div className='issues'>
+    
+    </div>
+  )
+}
+
+export default Issues
