cf691528ab18c0fbf9249c862720b6beba936485
Luke Olali <mugapedia@gmail.com>
2022-05-04 16:29:26 +0300

    specifies styling for IssueItem component and links style file to module in which IssueItem is defined

--- src/Issue/IssueItem/index.js
+++ src/Issue/IssueItem/index.js
@@ -1,5 +1,7 @@
 import Link from "../../Link"
 
+import "./style.css"
+
 const IssueItem = ({
   bodyHTML,
   number,
@@ -8,9 +10,11 @@ const IssueItem = ({
   url
 }) => {
    return (
-     <div>
-        <h3><Link href={url}>&#35;{number}{' '}[{state}] - {title}</Link></h3>
-        <div dangerouslySetInnerHTML={{ __html: bodyHTML }}></div>
+     <div className="IssueItem">
+        <div className="IssueItem-content">
+          <h3><Link href={url}>&#35;{number}{' '}[{state}] - {title}</Link></h3>
+          <div dangerouslySetInnerHTML={{ __html: bodyHTML }}></div>
+        </div>
      </div>
    )
 }
